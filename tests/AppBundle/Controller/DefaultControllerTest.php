7693651ac2beeb7101556a30150740bb227e7a20
Anthony Van de Gejuchte <anthonyvdgent@gmail.com>
2017-06-11 23:31:06 +0200

    Add basic tests

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -3,9 +3,17 @@
 namespace Tests\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Tests\AppBundle\DatabasePrimer;
 
 class DefaultControllerTest extends WebTestCase
 {
+    public function setUp()
+    {
+        self::bootKernel();
+
+        DatabasePrimer::prime(self::$kernel);
+    }
+
     public function testIndex()
     {
         $client = static::createClient();
@@ -13,6 +21,28 @@ class DefaultControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $this->assertContains('Hello guest,', $crawler->filter('.container')->text());
+    }
+
+    public function testLogin()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/login');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertContains('Voting board', $crawler->filter('h1')->text());
+        $this->assertContains('Login', $crawler->filter('h2')->text());
+    }
+
+    public function testRegistration()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/registration');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertContains('Voting board', $crawler->filter('h1')->text());
+        $this->assertContains('User registration', $crawler->filter('h2')->text());
     }
 }
