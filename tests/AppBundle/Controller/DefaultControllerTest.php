ba36f1ad2aa676043f4b086b4e9c9ea57a90c8de
Anthony Van de Gejuchte <anthonyvdgent@gmail.com>
2017-06-12 11:29:17 +0200

    Add simple database checks

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -12,37 +12,53 @@ class DefaultControllerTest extends WebTestCase
         self::bootKernel();
 
         DatabasePrimer::prime(self::$kernel);
+        DatabasePrimer::truncateAll(self::$kernel);
     }
 
     public function testIndex()
     {
         $client = static::createClient();
+        $client->enableProfiler();
 
         $crawler = $client->request('GET', '/');
 
+        // Check content
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Hello guest,', $crawler->filter('.container')->text());
+
+        // Check database queries - Need at least one to fetch the polls
+        $this->assertGreaterThan(0, $client->getProfile()->getCollector('db')->getQueryCount());
     }
 
     public function testLogin()
     {
         $client = static::createClient();
+        $client->enableProfiler();
 
         $crawler = $client->request('GET', '/login');
 
+        // Check content
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Voting board', $crawler->filter('h1')->text());
         $this->assertContains('Login', $crawler->filter('h2')->text());
+
+        // Check database queries - No database access required
+        $this->assertEquals(0, $client->getProfile()->getCollector('db')->getQueryCount());
     }
 
     public function testRegistration()
     {
         $client = static::createClient();
+        $client->enableProfiler();
 
         $crawler = $client->request('GET', '/registration');
 
+        // Check content
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Voting board', $crawler->filter('h1')->text());
         $this->assertContains('User registration', $crawler->filter('h2')->text());
+
+        // Check database queries - No database access required
+        $this->assertEquals(0, $client->getProfile()->getCollector('db')->getQueryCount());
     }
 }
--- tests/AppBundle/DatabasePrimer.php
+++ tests/AppBundle/DatabasePrimer.php
@@ -1,5 +1,6 @@
 <?php
-// From https://www.sitepoint.com/quick-tip-testing-symfony-apps-with-a-disposable-database/
+// Prime class and prime static method from
+// https://www.sitepoint.com/quick-tip-testing-symfony-apps-with-a-disposable-database/
 
 namespace Tests\AppBundle;
 
@@ -25,5 +26,31 @@ class DatabasePrimer
         $schemaTool->updateSchema($metadatas);
 
         // If you are using the Doctrine Fixtures Bundle you could load these here
+    }
+
+    public static function truncateAll(KernelInterface $kernel)
+    {
+        // Make sure we are in the test environment
+        if ('test' !== $kernel->getEnvironment()) {
+            throw new \LogicException('Primer must be executed in the test environment');
+        }
+
+        // Get the entity and schema manager from the service container
+        $entityManager = $kernel->getContainer()->get('doctrine.orm.entity_manager');
+        $schemaManager = $kernel->getContainer()
+            ->get('doctrine.dbal.default_connection')
+            ->getSchemaManager();
+
+        // Get all tables
+        $tables = $schemaManager->listTableNames();
+
+        // Get more required stuff
+        $connection = $entityManager->getConnection();
+        $platform   = $connection->getDatabasePlatform();
+
+        // Truncate all tables
+        foreach ($tables as $table) {
+            $connection->executeUpdate($platform->getTruncateTableSQL($table, false /* whether to cascade */));
+        }
     }
 }
